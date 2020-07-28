fdc2b0e70c3b57a7cf1f31aa6d182d026dc5f1a2
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-28 23:13:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,6 +44,8 @@ class Song
   def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3")
     split_filename.split(" - ")
+    song = self.new
+    song.artist_name = split_filename[0, 1]
   end
 
 end
