c1a83014477169dc6856cc0496bc6003c4afe837
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-28 23:12:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,9 +42,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    array = filename.split(" - ")
-    song = self.new
-    name = array[0], array[1]
+    split_filename = filename.chomp(".mp3")
   end
 
 end
